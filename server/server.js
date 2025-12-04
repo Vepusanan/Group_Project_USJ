1dd8d06862215558bd873f04decd1dcfcb63599a
Tharindu <tharinduonline1080@gmail.com>
2025-12-04 15:10:05 +0530

    comment

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(helmet());
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); //body parser
 app.use(express.urlencoded({ extended: true }));
 
 // Expose uploads for local testing (POSTMAN). In production use S3 or equivalent.
