cf50dd91d3c36184f6bb6016ffb3a6d9883182cf
Tharindu-X <190408711+Tharindu-X@users.noreply.github.com>
2025-12-14 00:25:35 +0530

    done story 6.1

---
+++ server/routes/account.js
@@ -0,0 +1,18 @@
+import express from "express";
+import {
+  changeEmail,
+  verifyEmailChange,
+  changePassword,
+  deleteAccount,
+} from "../controllers/accountController.js";
+import { protect } from "../middleware/auth.js";
+
+const router = express.Router();
+
+// All account routes require authentication
+router.put("/email", protect, changeEmail);
+router.get("/verify-email-change", verifyEmailChange);
+router.put("/password", protect, changePassword);
+router.delete("/", protect, deleteAccount);
+
+export default router;
